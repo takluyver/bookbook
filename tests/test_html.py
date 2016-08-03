fcd42671b4e9caf01408925d6e55d738b95317c9
Thomas Kluyver <thomas@kluyver.me.uk>
2016-08-03 17:23:39 +0100

    More tests

--- tests/test_html.py
+++ tests/test_html.py
@@ -12,3 +12,18 @@ def test_sampledir():
         html.convert_directory(sample_dir, td)
 
         assert_isfile(td / '01-introduction.html')
+
+def test_convert_link():
+    sample = "[link](01-abc.ipynb)"
+    res = html.markdown2html_custom(sample)
+    assert '01-abc.html' in res
+    assert '.ipynb' not in res
+
+    sample = "[link](02-def.ipynb#foo)"
+    res = html.markdown2html_custom(sample)
+    assert '02-def.html#foo' in res
+    assert '.ipynb' not in res
+
+    # Links to external sites shouldn't be converted
+    sample = "[link](http://example.com/01-abc.ipynb)"
+    assert '01-abc.ipynb' in html.markdown2html_custom(sample)
--- tests/test_latex.py
+++ tests/test_latex.py
@@ -12,3 +12,20 @@ def test_sampledir():
         latex.combine_and_convert(sample_dir, td / 'combined.pdf', pdf=True)
 
         assert_isfile(td / 'combined.pdf')
+
+
+def test_convert_link():
+    sample = "[link](01-abc.ipynb)"
+    res = latex.pandoc_convert_links(sample)
+    assert '\\ref{sec:01-abc}' in res
+    assert '.ipynb' not in res
+
+
+    sample = "[link](02-def.ipynb#Foo-bar)"
+    res = latex.pandoc_convert_links(sample)
+    assert '\\ref{foo-bar}' in res
+    assert '.ipynb' not in res
+
+    # Links to external sites shouldn't be converted
+    sample = "[link](http://example.com/01-abc.ipynb)"
+    assert '01-abc.ipynb' in latex.pandoc_convert_links(sample)
